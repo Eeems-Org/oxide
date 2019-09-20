c936447a5b54b92135e116e25aa4f6dd87350d46
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2019-09-20 15:39:26 -0600

    Update to the newest toolchain

--- docker-toolchain/qtcreator/files/start-qt.sh
+++ docker-toolchain/qtcreator/files/start-qt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source /opt/poky/2.1.3/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
+source /opt/poky/next/environment-setup-cortexa9hf-neon-poky-linux-gnueabi
 exec qtcreator
