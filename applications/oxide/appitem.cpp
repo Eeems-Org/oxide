0888411e56720516c8e46a7f90c3d4d50369a692
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2019-09-24 13:58:57 -0600

    Get oxide to handle term config. Update fingerterm UI

--- applications/oxide/appitem.cpp
+++ applications/oxide/appitem.cpp
@@ -1,9 +1,17 @@
+#include <QDebug>
 #include "appitem.h"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
 
 bool AppItem::ok(){
     return !_name.isEmpty() && !_call.isEmpty();
 }
 
 void AppItem::execute(){
+    qDebug() << "Setting termfile /opt/etc/draft/.terminate";
+    std::ofstream termfile;
+    termfile.open("/opt/etc/draft/.terminate");
+    termfile << _term.toStdString() << std::endl;
     system(_call.toUtf8());
 }
