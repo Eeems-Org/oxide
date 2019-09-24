ff71a21ef60a0be07101d71bc652bc3e3cf23b2b
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2019-09-24 17:17:14 -0600

    Begin adding in exclusive button locks

--- applications/button-capture/main.cpp
+++ applications/button-capture/main.cpp
@@ -4,10 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <unordered_map>
 #include <sys/time.h>
+#include <ext/stdio_filebuf.h>
 
 using namespace std;
 
@@ -19,9 +21,27 @@ struct PressRecord {
     PressRecord (string name) : name(name) {}
     PressRecord(){}
 };
+//
+struct event_device {
+    const char *device;
+    int fd;
+    event_device(string path){
+        device = path.c_str();
+        fd = open(device, O_RDONLY);
+    }
+};
 
-int main()
-{
+const event_device evdev("/dev/input/event2");
+
+void exit_handler(){
+    // Release lock
+    if(!ioctl(evdev.fd, EVIOCGRAB, 0)){
+        cout << "Failed to release lock" << endl;
+    }
+    close(evdev.fd);
+}
+
+int main(){
     // Mapping the correct button IDs.
     unordered_map<int, PressRecord> map;
     map[105] = PressRecord("Left");
@@ -29,16 +49,39 @@ int main()
     map[106] = PressRecord("Right");
     map[116] = PressRecord("Power");
 
+    // Get event devices
+    if(evdev.fd == -1){
+        cout << "Failed to open event device: " << evdev.device << endl;
+        return EXIT_FAILURE;
+    }
+    // Aquire lock
+    char name[256];
+    int result = 0;
+    memset(name, 0, sizeof(name));
+    result = ioctl(evdev.fd, EVIOCGNAME(sizeof(name)), name);
+    cout << "Reading From : " << evdev.device << " (" << name << ")" << endl;
+
+    cout << "Getting exclusive access... " << endl;
+    result = ioctl(evdev.fd, EVIOCGRAB, 1);
+    if(result == EBUSY){
+        cout << "Device is busy" << endl;
+        return result;
+    }else if(result != 0){
+        cout << "Unknown error" << endl;
+        return result;
+    }
+    result = atexit(exit_handler);
+    if(result != 0){
+        exit_handler();
+        cout << "Unable to register exit handler" << endl;
+        return result;
+    }
+
     // Open the button device.
-    ifstream eventsfile;
-    eventsfile.open("/dev/input/event2", ios::in);
-    if(eventsfile.is_open()) {
-        cout << "File open for reading." << endl;
-    }
-    else {
-        cout << "File couldn't be opened." << endl;
-        return 0;
-    }
+//    ifstream eventsfile;
+//    eventsfile.open("/dev/input/event2", ios::in);
+    __gnu_cxx::stdio_filebuf<char> filebuf(evdev.fd, std::ios::in);
+    istream eventsfile(&filebuf);
 
     // Get the size of an input event in the right format!
     input_event ie;
@@ -89,10 +132,7 @@ int main()
             }
         }
     }
-
-    eventsfile.close();
-
     cout << "Quitting." << endl;
 
-    return 0;
+    return EXIT_SUCCESS;
 }
