8c65dc996c08ac7206336b4297504ae239ea9506
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2021-01-04 11:58:21 -0700

    Fix permissions and add logging

--- applications/system-service/apibase.cpp
+++ applications/system-service/apibase.cpp
@@ -5,14 +5,18 @@ int APIBase::hasPermission(QString permission){
     if(getpgid(getpid()) == getSenderPgid()){
         return true;
     }
+    qDebug() << "Checking permission" << permission;
     for(auto name : appsAPI->runningApplications().keys()){
         auto app = appsAPI->getApplication(name);
         if(app == nullptr){
             continue;
         }
         if(app->processId() == getSenderPgid()){
-
+            auto result = app->permissions().contains(permission);
+            qDebug() << app->name() << result;
+            return result;
         }
     }
+    qDebug() << "app not found, permission granted";
     return true;
 }
