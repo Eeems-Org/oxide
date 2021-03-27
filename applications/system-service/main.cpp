e433b83a01af778b963eb7d2ab3b056b8a020ae0
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2021-03-27 12:40:23 -0600

    Feature/detect no rm2fb (#185)
    Fix #173

--- applications/system-service/main.cpp
+++ applications/system-service/main.cpp
@@ -16,6 +16,10 @@ void sigHandler(int signal){
 }
 
 int main(int argc, char *argv[]){
+    if(deviceSettings.getDeviceType() == DeviceSettings::RM2 && getenv("RM2FB_ACTIVE") == nullptr){
+        qWarning() << "rm2fb not detected. Running xochitl instead!";
+        return QProcess::execute("/usr/bin/xochitl");
+    }
     if (strcmp(qt_version, QT_VERSION_STR) != 0){
         qDebug() << "Version mismatch, Runtime: " << qt_version << ", Build: " << QT_VERSION_STR;
     }
