7bbe162162320d71f389ea844f74d8d5ab0c2911
Ben Siraphob <bensiraphob@gmail.com>
2022-01-10 21:45:19 +0700

    Adjust Nix build to use oxide target

--- oxide.nix
+++ oxide.nix
@@ -21,7 +21,28 @@ stdenv.mkDerivation {
   makeFlags = [ "release" ];
 
   installPhase = ''
-    cp -r release/. $out
+    install -D -m 755 -t $out/opt/bin release/opt/bin/erode
+    install -D -m 644 -t $out/opt/etc/draft/icons release/opt/etc/draft/icons/erode.svg
+    install -D -m 644 -t $out/opt/etc/draft/icons release/opt/etc/draft/icons/erode-splash.png
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.erode.oxide
+    install -D -m 755 -t $out/opt/bin release/opt/bin/fret
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.fret.oxide
+    install -D -m 755 -t $out/opt/bin release/opt/bin/oxide
+    install -D -m 644 -t $out/opt/etc release/opt/etc/oxide.conf
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.oxide.oxide
+    install -D -m 644 -t $out/opt/etc/draft/icons release/opt/etc/draft/icons/oxide-splash.png
+    install -D -m 755 -t $out/opt/bin release/opt/bin/rot
+    install -D -m 644 -t $out/etc/dbus-1/system.d release/etc/dbus-1/system.d/codes.eeems.oxide.conf
+    install -D -m 644 -t $out/lib/systemd/system release/etc/systemd/system/tarnish.service
+    install -D -m 755 -t $out/opt/bin release/opt/bin/tarnish
+    install -D -m 755 -t $out/opt/bin release/opt/bin/decay
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.decay.oxide
+    install -D -m 755 -t $out/opt/bin release/opt/bin/corrupt
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.corrupt.oxide
+    install -D -m 755 -t $out/opt/bin release/opt/bin/anxiety
+    install -D -m 644 -t $out/opt/usr/share/applications release/opt/usr/share/applications/codes.eeems.anxiety.oxide
+    install -D -m 644 -t $out/opt/etc/draft/icons release/opt/etc/draft/icons/image.svg
+    install -D -m 644 -t $out/opt/etc/draft/icons release/opt/etc/draft/icons/anxiety-splash.png
   '';
 
   meta = with lib; {
