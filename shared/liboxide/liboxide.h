ce77f11eb066d692467ac724e7348a1b080ed445
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2022-05-21 15:00:37 -0600

    Start getting doxygen rendering properly

--- shared/liboxide/liboxide.h
+++ shared/liboxide/liboxide.h
@@ -1,5 +1,12 @@
+/*!
+ * \file liboxide.h
+ */
 #ifndef LIBOXIDE_H
 #define LIBOXIDE_H
+
+#ifdef DOXYGEN_SHOULD_SKIP_THIS
+#define SENTRY
+#endif
 
 #include "liboxide_global.h"
 
@@ -32,8 +39,20 @@
 #define OXIDE_NOTIFICATION_INTERFACE OXIDE_SERVICE ".Notification"
 #define OXIDE_SCREENSHOT_INTERFACE OXIDE_SERVICE ".Screenshot"
 
+/*!
+ * \def deviceSettings()
+ * \brief Get the Oxide::DeviceSettings instance
+ */
 #define deviceSettings Oxide::DeviceSettings::instance()
+/*!
+ * \def xochitlSettings()
+ * \brief Get the Oxide::XochitlSettings instance
+ */
 #define xochitlSettings Oxide::XochitlSettings::instance()
+/*!
+ * \def sharedSettings()
+ * \brief Get the Oxide::SharedSettings instance
+ */
 #define sharedSettings Oxide::SharedSettings::instance()
 
 #ifdef SENTRY
@@ -41,14 +60,31 @@
 #include <sentry/src/sentry_tracing.h>
 #endif
 
+/*!
+ * \brief Wifi Network definition
+ */
 typedef QMap<QString, QVariantMap> WifiNetworks;
 Q_DECLARE_METATYPE(WifiNetworks);
-
+/*!
+ * \addtogroup Oxide
+ * \brief The main Oxide namespace
+ * @{
+ */
+/*!
+ * \abstract
+ */
 namespace Oxide {
+    /*!
+     * \brief Run code on the main Qt thread
+     * \param The code to run on the main thread
+     */
     LIBOXIDE_EXPORT void dispatchToMainThread(std::function<void()> callback);
     namespace Sentry{
         struct Transaction {
 #ifdef SENTRY
+            /*!
+             * \brief The sentry_transaction_t instance
+             */
             sentry_transaction_t* inner;
             explicit Transaction(sentry_transaction_t* t);
 #else
@@ -58,6 +94,9 @@ namespace Oxide {
         };
         struct Span {
 #ifdef SENTRY
+            /*!
+             * \brief The sentry_span_t instance
+             */
             sentry_span_t* inner;
             explicit Span(sentry_span_t* s);
 #else
@@ -65,7 +104,6 @@ namespace Oxide {
             explicit Span(void* s);
 #endif
         };
-
         LIBOXIDE_EXPORT const char* bootId();
         LIBOXIDE_EXPORT const char* machineId();
         LIBOXIDE_EXPORT void sentry_init(const char* name, char* argv[], bool autoSessionTracking = true);
@@ -82,17 +120,63 @@ namespace Oxide {
         LIBOXIDE_EXPORT void sentry_span(Span* parent, std::string operation, std::string description, std::function<void(Span* span)> callback);
         LIBOXIDE_EXPORT void trigger_crash();
     }
+    /*!
+     * \brief Device specific values
+     */
     class LIBOXIDE_EXPORT DeviceSettings{
     public:
-        enum DeviceType { Unknown, RM1, RM2 };
+        /*!
+         * \brief Known device types
+         */
+        enum DeviceType {
+            Unknown, /*!< Unknown device type >*/
+            RM1, /*!< reMarkable 1 >*/
+            RM2 /*!< reMarkable 2 >*/
+        };
+        /*!
+         * \brief Get the static instance of this class. You should use the deviceSettings macro instead.
+         * \return The static instance
+         */
         static DeviceSettings& instance();
+        /*!
+         * \brief Get the path to the buttons input device
+         * \return Path to the buttons device
+         */
         const char* getButtonsDevicePath() const;
+        /*!
+         * \brief Get the path to the wacom input device
+         * \return Path to the wacom device
+         */
         const char* getWacomDevicePath() const;
+        /*!
+         * \brief Get the path to the touch input device
+         * \return Path to the touch device
+         */
         const char* getTouchDevicePath() const;
+        /*!
+         * \brief Get the Qt environment settings for the device
+         * \return The Qt environment settings for the device
+         */
         const char* getTouchEnvSetting() const;
+        /*!
+         * \brief Get the device type
+         * \return The device type
+         */
         DeviceType getDeviceType() const;
+        /*!
+         * \brief Get the human readable device name
+         * \return Human readable device name
+         */
         const char* getDeviceName() const;
+        /*!
+         * \brief Get the max width for touch input on the device
+         * \return Max width for touch input
+         */
         int getTouchWidth() const;
+        /*!
+         * \brief Get the max height for touch input on the device
+         * \return Max height for touch input
+         */
         int getTouchHeight() const;
 
     private:
@@ -106,7 +190,9 @@ namespace Oxide {
         std::string wacomPath = "";
         std::string touchPath = "";
     };
-
+    /*!
+     * \brief Manage Xochitl settings
+     */
     class LIBOXIDE_EXPORT XochitlSettings : public SettingsFile {
         Q_OBJECT
         // cppcheck-suppress uninitMemberVarPrivate
@@ -127,7 +213,9 @@ namespace Oxide {
         ~XochitlSettings();
         WifiNetworks m_wifinetworks;
     };
-
+    /*!
+     * \brief Shared settings for Oxide
+     */
     class LIBOXIDE_EXPORT SharedSettings : public SettingsFile {
         Q_OBJECT
         // cppcheck-suppress uninitMemberVarPrivate
@@ -141,5 +229,5 @@ namespace Oxide {
         ~SharedSettings();
     };
 }
-
+/*! @} */
 #endif // LIBOXIDE_H
