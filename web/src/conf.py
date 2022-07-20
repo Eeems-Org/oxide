68ea2f13b83be06101ecca8e7d378490edea0c26
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2022-07-20 09:05:35 -0600

    Update copyright

--- web/src/conf.py
+++ web/src/conf.py
@@ -1,5 +1,8 @@
+import datetime
+year = datetime.datetime.today().year
+
 project = "Oxide"
-copyright = "2021, Eeems"
+copyright = f"2021-{year}, Eeems"
 author = "Nathaniel 'Eeems' van Diepen"
 
 html_theme_path = ["_themes"]
