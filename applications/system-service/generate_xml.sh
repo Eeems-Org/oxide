9ea21fe12b4a9194f7a0d7bf308adea38a93cce8
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2023-01-27 20:29:31 -0700

    Add missing quotes

--- applications/system-service/generate_xml.sh
+++ applications/system-service/generate_xml.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")"
 mkdir -p ../../interfaces
 p(){
   echo "qdbuscpp2xml $1"
-  qdbuscpp2xml -A "$1" -o ../../interfaces/"$(basename $1 .h)".xml
+  qdbuscpp2xml -A "$1" -o ../../interfaces/"$(basename "$1" .h)".xml
 }
 
 p dbusservice.h
