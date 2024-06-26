8adbfaefa1fe0bff1d278ec44a0e35f3fe48b5d8
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2024-06-26 16:29:45 -0600

    Bump version number

--- shared/liboxide/meta.h
+++ shared/liboxide/meta.h
@@ -30,7 +30,7 @@
  * \def OXIDE_VERSION
  * \brief Version of Tarnish and liboxide
  */
-#define OXIDE_VERSION "2.8.3"
+#define OXIDE_VERSION "2.8.4"
 /*!
  * \def OXIDE_INTERFACE_VERSION
  * \brief Version of Tarnish and liboxide for dbus
