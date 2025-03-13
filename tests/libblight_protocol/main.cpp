ca382e36e8ff3c32d8b12f0dd5f50975c9e025f8
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2025-03-12 20:49:07 -0600

    Don't run C tests if in a vm

--- tests/libblight_protocol/main.cpp
+++ tests/libblight_protocol/main.cpp
@@ -8,47 +8,57 @@
 #include <libblight/dbus.h>
 #include "test.h"
 
+int run_c_tests(QCoreApplication* app){
+    QProcess blight;
+    qDebug() << "Checking to see if blight is running...";
+    auto dbus = new Blight::DBus(
+#ifdef EPAPER
+      true
+#else
+      false
+#endif
+      );
+    if(!dbus->has_service(BLIGHT_SERVICE)){
+        blight.start("/opt/bin/blight", QStringList());
+        qDebug() << "Waiting for blight to start...";
+        if(!blight.waitForStarted()){
+            qDebug() << "Failed to start: " << blight.exitCode();
+            return 1;
+        }
+        qDebug() << "Waiting for blight dbus service...";
+        while(!dbus->has_service(BLIGHT_SERVICE)){
+            app->processEvents(QEventLoop::AllEvents, 1000);
+            if(blight.state() == QProcess::NotRunning){
+                qDebug() << "Blight failed to start:" << blight.exitCode();
+                return blight.exitCode() || 1;
+            }
+        }
+    }
+    delete dbus;
+    test_c();
+    if(blight.state() != QProcess::NotRunning){
+        qDebug() << "Waiting for blight to stop...";
+        blight.kill();
+        blight.waitForFinished();
+    }
+    return 0;
+}
+
 int main(int argc, char* argv[]){
     QThread::currentThread()->setObjectName("main");
     QCoreApplication app(argc, argv);
     app.setAttribute(Qt::AA_Use96Dpi, true);
     QTimer::singleShot(0, [&app, argc, argv]{
-        QProcess blight;
-        qDebug() << "Checking to see if blight is running...";
-        auto dbus = new Blight::DBus(
-#ifdef EPAPER
-          true
-#else
-          false
-#endif
-        );
-        if(!dbus->has_service(BLIGHT_SERVICE)){
-            blight.setEnvironment(QStringList() << "RM2FB_ACTIVE=1");
-            blight.start("/opt/bin/blight", QStringList());
-            qDebug() << "Waiting for blight to start...";
-            if(!blight.waitForStarted()){
-                qDebug() << "Failed to start: " << blight.exitCode();
-                app.exit(1);
+        if(!QProcess::execute("systemd-detect-virt", QStringList() << "--quiet")){
+            int res = run_c_tests(&app);
+            if(res){
+                app.exit(res);
+                return;
             }
-            qDebug() << "Waiting for blight dbus service...";
-            while(!dbus->has_service(BLIGHT_SERVICE)){
-                app.processEvents(QEventLoop::AllEvents, 1000);
-                if(blight.state() == QProcess::NotRunning){
-                    qDebug() << "Blight failed to start:" << blight.exitCode();
-                    app.exit(blight.exitCode() || 1);
-                    return;
-                }
-            }
+        }else{
+            qDebug() << "Skipping C tests, we are in a VM...";
         }
-        delete dbus;
-        test_c();
-        int res = AutoTest::run(argc, argv);
-        if(blight.state() != QProcess::NotRunning){
-            qDebug() << "Waiting for blight to stop...";
-            blight.kill();
-            blight.waitForFinished();
-        }
-        app.exit(res);
+        app.exit(AutoTest::run(argc, argv));
     });
     return app.exec();
 }
