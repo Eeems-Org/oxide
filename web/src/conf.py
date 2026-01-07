216b2744e8efb91aaa2295651c179b7ae0073078
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-01-06 19:00:39 -0700

    Better dark theme

--- web/src/conf.py
+++ web/src/conf.py
@@ -1,4 +1,5 @@
 import datetime
+
 year = datetime.datetime.today().year
 
 project = "Oxide"
@@ -14,5 +15,17 @@ html_theme = "oxide"
 master_doc = "sitemap"
 html_sidebars = {"**": ["nav.html", "sidefooter.html"]}
 html_permalinks_icon = "#"
+html_css_files = ["oxide.css"]
+html_js_files = [
+    "https://peek.eeems.website/peek.js",
+    "oxide.js",
+    (
+        "https://browser.sentry-cdn.com/10.20.0/bundle.tracing.min.js",
+        {
+            "integrity": "sha384-+9wUXHfFUeGwAnEesSxSFwKwBHDbnc32jfpX3HN0aB9RHa4lbS1rqsSnM6DI9/LM",
+            "crossorigin": "anonymous",
+        },
+    ),
+]
 
 extensions = ["sphinxcontrib.fulltoc", "breathe"]
