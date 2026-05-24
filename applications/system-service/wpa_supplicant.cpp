9e10cb774197290b6685148511521dd636068a7d
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-05-24 13:40:28 -0600

    Format

--- applications/system-service/wpa_supplicant.cpp
+++ applications/system-service/wpa_supplicant.cpp
@@ -15,34 +15,63 @@
  * Implementation of interface class FiW1Wpa_supplicant1Interface
  */
 
-FiW1Wpa_supplicant1Interface::FiW1Wpa_supplicant1Interface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent)
-    : QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
+FiW1Wpa_supplicant1Interface::FiW1Wpa_supplicant1Interface(
+    const QString& service,
+    const QString& path,
+    const QDBusConnection& connection,
+    QObject* parent
+)
+  : QDBusAbstractInterface(
+        service,
+        path,
+        staticInterfaceName(),
+        connection,
+        parent
+    )
 {
 }
 
-FiW1Wpa_supplicant1Interface::~FiW1Wpa_supplicant1Interface()
-{
-}
+FiW1Wpa_supplicant1Interface::~FiW1Wpa_supplicant1Interface() {}
 
 /*
  * Implementation of interface class FiW1Wpa_supplicant1BSSInterface
  */
 
-FiW1Wpa_supplicant1BSSInterface::FiW1Wpa_supplicant1BSSInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent)
-    : QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
+FiW1Wpa_supplicant1BSSInterface::FiW1Wpa_supplicant1BSSInterface(
+    const QString& service,
+    const QString& path,
+    const QDBusConnection& connection,
+    QObject* parent
+)
+  : QDBusAbstractInterface(
+        service,
+        path,
+        staticInterfaceName(),
+        connection,
+        parent
+    )
 {
 }
 
-FiW1Wpa_supplicant1BSSInterface::~FiW1Wpa_supplicant1BSSInterface()
-{
-}
+FiW1Wpa_supplicant1BSSInterface::~FiW1Wpa_supplicant1BSSInterface() {}
 
 /*
  * Implementation of interface class FiW1Wpa_supplicant1InterfaceInterface
  */
 
-FiW1Wpa_supplicant1InterfaceInterface::FiW1Wpa_supplicant1InterfaceInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent)
-    : QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
+FiW1Wpa_supplicant1InterfaceInterface::FiW1Wpa_supplicant1InterfaceInterface(
+    const QString& service,
+    const QString& path,
+    const QDBusConnection& connection,
+    QObject* parent
+)
+  : QDBusAbstractInterface(
+        service,
+        path,
+        staticInterfaceName(),
+        connection,
+        parent
+    )
 {
 }
 
@@ -54,12 +83,20 @@ FiW1Wpa_supplicant1InterfaceInterface::~FiW1Wpa_supplicant1InterfaceInterface()
  * Implementation of interface class FiW1Wpa_supplicant1NetworkInterface
  */
 
-FiW1Wpa_supplicant1NetworkInterface::FiW1Wpa_supplicant1NetworkInterface(const QString &service, const QString &path, const QDBusConnection &connection, QObject *parent)
-    : QDBusAbstractInterface(service, path, staticInterfaceName(), connection, parent)
+FiW1Wpa_supplicant1NetworkInterface::FiW1Wpa_supplicant1NetworkInterface(
+    const QString& service,
+    const QString& path,
+    const QDBusConnection& connection,
+    QObject* parent
+)
+  : QDBusAbstractInterface(
+        service,
+        path,
+        staticInterfaceName(),
+        connection,
+        parent
+    )
 {
 }
 
-FiW1Wpa_supplicant1NetworkInterface::~FiW1Wpa_supplicant1NetworkInterface()
-{
-}
-
+FiW1Wpa_supplicant1NetworkInterface::~FiW1Wpa_supplicant1NetworkInterface() {}
