f98cf89d0fe8f724930218b5a7c34611e014a551
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2026-07-13 20:25:08 -0600

    Fix crash on restart

--- shared/qpa/oxideintegration.cpp
+++ shared/qpa/oxideintegration.cpp
@@ -134,13 +134,10 @@ OxideIntegration::initialize() {
   qApp->installEventFilter(new OxideEventFilter(qApp));
   m_inputContext = QPlatformInputContextFactory::create();
   m_eventManager = new OxideEventManager(m_parameters);
-  connect(qApp, &QGuiApplication::aboutToQuit, [this] {
-    if (m_eventManager == nullptr) {
-      return;
-    }
+  connect(qApp, &QGuiApplication::aboutToQuit, m_eventManager, [this] {
     delete m_eventManager;
     m_eventManager = nullptr;
-  });
+  }, Qt::QueuedConnection);
 #ifndef QT_NO_CLIPBOARD
   auto compositor = new Compositor(
     BLIGHT_SERVICE,
